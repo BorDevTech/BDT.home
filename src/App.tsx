915ccad5a9bbaada782115939ce404f3c207ef6a
BorDevTech <devell.robinson@icloud.com>
2023-07-10 03:32:44 -0400

    added background color and Chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,40 @@
- 
 import "./App.css";
 import { Link, Routes, Route } from "react-router-dom";
 import * as pages from "./routes/index";
+import * as Chakra from "@chakra-ui/react";
 
-function App() { 
-
+function App() {
   return (
     <>
-      <nav>
-        <Link to={"/BDT.home"}>BorDev Tech</Link>
-        <Link to={"/BDT.home/about"}>About</Link>
-        <Link to={"/BDT.home/services"}>Services</Link>
-        <Link to={"/BDT.home/projects"}>Projects</Link>
-        <Link to={"/BDT.home/contact"}>Contact</Link>
-        <input placeholder="Search Bar"/>
-        <button type="button">Search Button</button>
-        <button type="button">Get Started Button</button> 
-        <button type="button">Dark/Light Mode Toggle</button>  
-      </nav>
-      <Routes>
-        <Route path={`/BDT.home`} element={<pages.Home />} />
-        <Route path={`/BDT.home/about`} element={<pages.About />} />
-        <Route path={`/BDT.home/services`} element={<pages.Services />} />
-        <Route path={`/BDT.home/projects`} element={<pages.Projects />} />
-        <Route path={`/BDT.home/contact`} element={<pages.Contact />} />
-        <Route path={`/BDT.home/index`} element={<pages.Home />} />
-      </Routes>
+      <Chakra.Box
+        w="100%"
+        h="200px"
+        bgGradient="linear(to-r, green.200, pink.500)"
+      >
+        <Chakra.Card>
+          <Chakra.CardBody>
+            <nav>
+              <Link to={"/BDT.home"}>BorDev Tech</Link>
+              <Link to={"/BDT.home/about"}>About</Link>
+              <Link to={"/BDT.home/services"}>Services</Link>
+              <Link to={"/BDT.home/projects"}>Projects</Link>
+              <Link to={"/BDT.home/contact"}>Contact</Link>
+              <input placeholder="Search Bar" />
+              <button type="button">Search Button</button>
+              <button type="button">Get Started Button</button>
+              <button type="button">Dark/Light Mode Toggle</button>
+            </nav>
+          </Chakra.CardBody>
+        </Chakra.Card>
+        <Routes>
+          <Route path={`/BDT.home`} element={<pages.Home />} />
+          <Route path={`/BDT.home/about`} element={<pages.About />} />
+          <Route path={`/BDT.home/services`} element={<pages.Services />} />
+          <Route path={`/BDT.home/projects`} element={<pages.Projects />} />
+          <Route path={`/BDT.home/contact`} element={<pages.Contact />} />
+          <Route path={`/BDT.home/index`} element={<pages.Home />} />
+        </Routes>
+      </Chakra.Box>
     </>
   );
 }
