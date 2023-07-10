65ecf41bd18db1a246db05031d9d66c0d7a98446
BorDevTech <devell.robinson@icloud.com>
2023-07-10 14:12:26 -0400

    removed height from grid and allowed local expose in dev for responsive development view

--- src/App.tsx
+++ src/App.tsx
@@ -6,39 +6,30 @@ import * as Chakra from "@chakra-ui/react";
 function App() {
   return (
     <>
-      <Chakra.Grid
-        h={window.innerHeight}
+      <Chakra.Grid 
         w={window.innerWidth}
         templateAreas={{
           base: `"navbar" "main" "footer"`,
           lg: `"navbar" "main" "footer"`,
         }}
         bg={"linear(to-r, green.200, pink.500)"}
+        gap={0}
       >
         <Chakra.GridItem
           area={"navbar"}
-          bgGradient="linear(to-r, green.200, pink.500)"
+          bgGradient="linear(to-r, green.200, pink.500)" 
         >
-          <Chakra.Box
-            w={`100%`}
-            h={`100%`} 
-          >
-            <Chakra.Card>
-              <Chakra.CardBody>
-                <nav>
-                  <Link to={"/BDT.home"}>BorDev Tech</Link>
-                  <Link to={"/BDT.home/about"}>About</Link>
-                  <Link to={"/BDT.home/services"}>Services</Link>
-                  <Link to={"/BDT.home/projects"}>Projects</Link>
-                  <Link to={"/BDT.home/contact"}>Contact</Link>
-                  <input placeholder="Search Bar" />
-                  <button type="button">Search Button</button>
-                  <button type="button">Get Started Button</button>
-                  <button type="button">Dark/Light Mode Toggle</button>
-                </nav>
-              </Chakra.CardBody>
-            </Chakra.Card>
-          </Chakra.Box>
+          <nav>
+            <Link to={"/BDT.home"}>BorDev Tech</Link>
+            <Link to={"/BDT.home/about"}>About</Link>
+            <Link to={"/BDT.home/services"}>Services</Link>
+            <Link to={"/BDT.home/projects"}>Projects</Link>
+            <Link to={"/BDT.home/contact"}>Contact</Link>
+            <input placeholder="Search Bar" />
+            <button type="button">Search Button</button>
+            <button type="button">Get Started Button</button>
+            <button type="button">Dark/Light Mode Toggle</button>
+          </nav>
         </Chakra.GridItem>
         <Routes>
           <Route path={`/BDT.home`} element={<pages.Home />} />
