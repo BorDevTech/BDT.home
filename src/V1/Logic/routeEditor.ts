0061729ef2edff896349d83aadce6ad332073896
BorDevTech <bordevtech@icloud.com>
2023-11-26 23:29:36 -0500

    bit for admin panel

---
+++ src/V1/Logic/routeEditor.ts
@@ -0,0 +1,17 @@
+const AddRoute = () => {
+  return;
+};
+
+const GetRoutes = () => {
+  return;
+};
+
+const UpdateRoute = () => {
+  return;
+};
+
+const RemoveRoute = () => {
+  return;
+};
+
+export { AddRoute, GetRoutes, UpdateRoute, RemoveRoute };
